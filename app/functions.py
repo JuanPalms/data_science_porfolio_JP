705937027b849075ca2164e1314b150aacb743d1
JuanPalms <palmerosjuanfrancisco@gmail.com>
2023-07-22 18:35:06 -0600

    Moved main.py to subfolder

--- app/functions.py
+++ app/functions.py
@@ -1,7 +1,27 @@
 """
 This python module implements some functions to be used in the main.py file.
 """
+import os
+import sys
 import base64
+
+CURRENT = os.path.dirname(os.path.abspath(__file__))
+ROOT = os.path.dirname(CURRENT)
+
+# Function to load yaml configuration file
+def load_config(config_name):
+    """
+    Sets the configuration file path
+    Args:
+    config_name: Name of the configuration file in the directory
+    Returns:
+    Configuration file
+    """
+    with open(os.path.join(ROOT, config_name), encoding="utf-8") as conf:
+        config = yaml.safe_load(conf)
+    return config
+
+
 # Function to encode the file to base64
 def get_binary_file_downloader_html(bin_file, file_label='File'):
     """
